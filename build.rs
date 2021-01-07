22f774775f64f0cec24ebc60d3cb6b04933f154b
kporika <kporika@ciena.com>
2021-01-06 20:51:15 -0500

    web sequence

--- build.rs
+++ build.rs
@@ -5,3 +5,16 @@ fn main() {
     // Generate the version.rs file in the Cargo OUT_DIR.
     assert!(vergen::generate_version_rs(flags).is_ok());
 }
+
+/*
+SNMP Data Collection - Rust - Actix - Actor Pattern
+Main Thread -> DB Server : Start Data Collection
+DB Server -> PostgresDB : Get POLLEDDATA
+DB Server->Snmp Sender: Get Attribute
+Snmp Sender -> Snmp Sender: Create PDU/Send
+Snmp Receiver -> Snmp Receiver: Decode PDU
+Snmp Receiver -> DB Server: Send Value
+Main Thread -> Main Thread  : Wait / Sleep
+Main Thread -> DB Server   : Initiate Dump
+DB Server -> PostgresDB : Bulk Copy Value(s)
+*/
